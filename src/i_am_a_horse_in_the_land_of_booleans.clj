c9346e5cb9fed74a2e3b431b4dde3d96488af39e
mark4asp <mark4asp@gmail.com>
2014-08-11 09:54:32 +0100

    Passing

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,41 +1,57 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
+(defn boolean
+  "is x a boolean?"
+  [x]
   (if x true
         false))
 
-(defn abs [x]
+(defn abs
+  "absolute value of x"
+  [x]
   (if (< x 0)
     (- x)
     x))
 
-(defn divides? [divisor n]
+(defn divides?
+  "is n divisable by divisor?"
+  [divisor n]
   (= (mod n divisor)
      0))
 
-(defn fizzbuzz [x]
+(defn fizzbuzz
+  "fizzbuzz"
+  [x]
   (cond (divides? 15 x) "gotcha!"
         (divides? 3 x)  "fizz"
         (divides? 5 x)  "buzz"
         :else           ""))
 
-(defn teen? [age]
+(defn teen?
+  "is age a teen? (13-19)"
+  [age]
   (and (>= age 13)
        (< age 20)))
 
-(defn not-teen? [age]
+(defn not-teen?
+  "is age a not-teen? (< 13 or > 19)"
+  [age]
   (or (< age 13)
       (> age 19)))
 
-(defn generic-doublificate [x]
+(defn generic-doublificate
+  "generic-doublificate - funny"
+  [x]
   (cond (number? x)      (* 2 x)
         (empty?  x)      nil
         (or (list? x)
             (vector? x)) (* 2 (count x))
         :else            true))
 
-(defn leap-year? [year]
+(defn leap-year?
+  "is year a leap-year?"
+  [year]
   (cond (divides? 400 year) true
         (divides? 100 year) false
         (divides? 4 year)   true
